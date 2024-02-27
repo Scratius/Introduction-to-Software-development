bce91f010404a0d1db5fa22e706adc3e8126df81
Юрий Алексеевич <scratius@MacBook-Pro-Yv1.local>
2024-02-27 08:30:01 +0300

    Add test

---
+++ build/CMakeFiles/HelloW.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloW.dir/src/main.cpp.o"
+  "CMakeFiles/HelloW.dir/src/main.cpp.o.d"
+  "HelloW"
+  "HelloW.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/HelloW.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HelloW.
+# This may be replaced when dependencies are built.
